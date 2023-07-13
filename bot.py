adf5d7de9e770306f611aa5b9f3a14b518211209
Romuch1not1first <rtkacenko9@gmail.com>
2023-07-13 23:04:00 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,38 @@
+import pyautogui
+import time
+import keyboard
+
+# The image you need to find on the screen
+img_path = 'image.png'
+
+# The location of the last click
+last_click_location = None
+
+# Key to stop the script when pressed
+stop_key = 'q'
+
+# Wait for the image to appear on the screen
+while True:
+    # Check if the key is pressed to stop
+    if keyboard.is_pressed(stop_key):
+        print("Script stopped by user.")
+        break
+
+    # Searching for an image on the screen
+    location = pyautogui.locateOnScreen(img_path, confidence=0.8)
+
+    # If an image is found, we click on its center
+    if location:
+        local_location = location
+        center = pyautogui.center(local_location)
+        pyautogui.click(center)
+
+        # Save the coordinates of the click location
+        last_click_location = center
+    else:
+        # If no image is found, click on the last click location if there is one
+        if last_click_location:
+            pyautogui.click(last_click_location)
+
+        print("The image was not found, let's try again..")
+        time.sleep(0.5)
